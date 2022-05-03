d2abc7e0b6011fb8a86fd0c02c08dc5ba1d71ff6
LGretzk <LGretzk@protonmail.com>
2022-05-03 22:26:12 +0100

    Doubles and stubbing practice

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,8 @@
 require 'docking_station'
 
 describe DockingStation do
+
+  let(:bike) { double('Bike') }
     
   it 'initializes with bikes array' do
     expect(subject.bikes).to eq []
@@ -12,9 +14,13 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'releases a bike that is working' do
-      subject.dock(Bike.new)
-      bike = subject.release_bike
-      expect(bike).to be_working  # Alternative: expect(bike.working?).to eq true
+    #allow(bike).to receive(:working?).and_return(true)
+    allow(bike).to receive(:broken?).and_return(false)
+    # Alternative: allow(bike).to receive(:working) {true}
+    # bike = double(:bike, broken?: true)
+    subject.dock(bike)
+    expect(subject.release_bike).to be bike
+    # Changed now: expect(released_bike).to be_working  # Alternative: expect(bike.working?).to eq true
   end
 
   it 'fails when there are none available' do
@@ -24,14 +30,13 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   it 'docks a bike' do
-    bike = Bike.new
     subject.dock(bike)
     expect(subject.bikes).to eq [bike]
   end
 
   it 'fails when docking station is full' do
-    20.times { subject.dock(Bike.new) }
-    expect { subject.dock(Bike.new) }.to raise_error 'Docking station full'
+    20.times { subject.dock(bike) }
+    expect { subject.dock(bike) }.to raise_error 'Docking station full'
   end
 
   it 'has default capacity' do
@@ -47,8 +52,7 @@ describe DockingStation do
   end
 
   it 'does not release broken bikes' do
-    bike = Bike.new
-    bike.report_broken
+    allow(bike).to receive(:broken?).and_return(true)
     subject.dock(bike)
     expect { subject.release_bike }.to raise_error 'No bikes available'
   end
